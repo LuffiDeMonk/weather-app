f6902567cfe1dd86ac143ca9b269e20633f971b3
LuffiDeMonk <119294283+LuffiDeMonk@users.noreply.github.com>
2023-06-14 19:13:51 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { Provider } from "react-redux";
-import { store } from "./components/features/store";
+
 import Home from "./components/Home";
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Home />
-        <ReactQueryDevtools position="bottom-left" initialIsOpen={false} />
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Home />
+      <ReactQueryDevtools position="bottom-left" initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
